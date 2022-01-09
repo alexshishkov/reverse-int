687108f16d90b0dd03052313f424dc413e0b72d5
Alexey Shishkov <53310068+alexshishkov@users.noreply.github.com>
2022-01-09 13:20:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    if (n > 0) {
+        return n.toString().split('').reverse().join("")
+    } else if ((n < 0)) {
+        let number = Math.abs(n)
+        return (number.toString().split('').reverse().join(""))
+    }
 }
